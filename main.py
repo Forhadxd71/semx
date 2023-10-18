2df17093daa771de7b26f7fd1ce8e552b9914aa6
Forhadxd71 <112680572+Forhadxd71@users.noreply.github.com>
2023-10-18 08:48:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from pyrogram import Client
+from pyrogram.api.errors import FloodWait
 
-# Replace these with your own API ID, API Hash, and phone number
+# Replace these with your own values
 api_id = "26936654"
 api_hash = "bbe46e790306860d1fc901e65d3e5327"
 phone_number = "+8801738230593"
 
-# Create a Pyrogram bot session
-with Client(":memory:", api_id=api_id, api_hash=api_hash, phone_number=phone_number) as app:
-    # Start the bot session to get the session string
-    session_string = app.export_session_string()
-    
-# Save the session string to a file (e.g., session.txt)
-with open("session.txt", "w") as file:
-    file.write(session_string)
+# Create a Pyrogram client
+app = Client(
+    "my_bot_session",
+    api_id=api_id,
+    api_hash=api_hash,
+    phone_number=phone_number,
+)
 
-print("Session file created successfully.")
+async def main():
+    await app.start()
+    me = await app.get_me()
+    print(f"Logged in as {me.username} ({me.id})")
+
+    # Save the session to a file
+    app.export_session("session.txt")
+
+    await app.send_message(
+        "me",  # Replace with the recipient's username or ID
+        "Bot session created and saved.",
+    )
+
+    await app.stop()
+
+if __name__ == "__main__":
+    with app:
+        app.loop.run_until_complete(main())
